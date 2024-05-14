059668257bbde1fc295bc372d4f711b230e7a207
Nguyen Tuan Duong <tuanduong152004@gmail.com>
2024-05-14 21:36:27 +0700

    refactor: change request get recommend

--- api/controllers/recommend.controller.go
+++ api/controllers/recommend.controller.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hodukihugi/winglets-api/services"
 	"github.com/hodukihugi/winglets-api/utils"
 	"net/http"
-	"strconv"
 )
 
 // RecommendController data type
@@ -77,66 +76,18 @@ func (c *RecommendController) GetUserMatches(ctx *gin.Context) {
 }
 
 func (c *RecommendController) GetRecommendations(ctx *gin.Context) {
-	var minAgeInt, maxAgeInt int
-	var minDistanceFloat, maxDistanceFloat float64
 	var err error
-
-	// Get query params
-	minAge, ok := ctx.GetQuery("min_age")
-	if !ok {
-		minAgeInt = 18
-	} else {
-		minAgeInt, err = strconv.Atoi(minAge)
-		if err != nil {
-			c.logger.Error(err)
-			ctx.JSON(http.StatusInternalServerError, models.HTTPResponse{
-				Message: "server error",
-			})
-			return
-		}
+	var request models.GetRecommendationRequest
+	if err = ctx.ShouldBindJSON(&request); err != nil {
+		c.logger.Error(err)
+		ctx.JSON(http.StatusBadRequest, models.HTTPResponse{
+			Message: err.Error(),
+		})
+		return
 	}
 
-	maxAge, ok := ctx.GetQuery("max_age")
-	if !ok {
-		maxAgeInt = 99
-	} else {
-		maxAgeInt, err = strconv.Atoi(maxAge)
-		if err != nil {
-			c.logger.Error(err)
-			ctx.JSON(http.StatusInternalServerError, models.HTTPResponse{
-				Message: "server error",
-			})
-			return
-		}
-	}
-
-	minDistance, ok := ctx.GetQuery("min_distance")
-	if !ok {
-		minDistanceFloat = 0
-	} else {
-		minDistanceFloat, err = strconv.ParseFloat(minDistance, 64)
-		if err != nil {
-			c.logger.Error(err)
-			ctx.JSON(http.StatusInternalServerError, models.HTTPResponse{
-				Message: "server error",
-			})
-			return
-		}
-	}
-
-	maxDistance, ok := ctx.GetQuery("max_distance")
-	if !ok {
-		maxDistanceFloat = 100
-	} else {
-		maxDistanceFloat, err = strconv.ParseFloat(maxDistance, 64)
-		if err != nil {
-			c.logger.Error(err)
-			ctx.JSON(http.StatusInternalServerError, models.HTTPResponse{
-				Message: "server error",
-			})
-			return
-		}
-	}
+	minAgeInt, maxAgeInt := request.MinAge, request.MaxAge
+	minDistanceFloat, maxDistanceFloat := request.MinDistance, request.MaxDistance
 
 	userID, err := utils.GetUserID(ctx)
 	if err != nil {
--- models/match.models.go
+++ models/match.models.go
@@ -6,3 +6,10 @@ type MatchCalculationResult struct {
 	MatchPercentage float64
 	MatchedProfile  Profile
 }
+
+type GetRecommendationRequest struct {
+	MinAge      int     `json:"minAge"`
+	MaxAge      int     `json:"maxAge"`
+	MinDistance float64 `json:"min_distance"`
+	MaxDistance float64 `json:"max_distance"`
+}
